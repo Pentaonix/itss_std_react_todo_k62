e049902fcad101127e30187a4d7d73d74e2df813
Pentaonix <ducquang0301@gmail.com>
2021-05-20 08:30:50 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjBaoRmJiHW_gwXDXbBvbEA7d-eRWj8-8",
+  authDomain: "fir-sample-c8b34.firebaseapp.com",
+  projectId: "fir-sample-c8b34",
+  storageBucket: "fir-sample-c8b34.appspot.com",
+  messagingSenderId: "329933421174",
+  appId: "1:329933421174:web:99f1d66dd6db8070ab3d57"
+};
+
+  firebase.initializeApp(firebaseConfig);
+  
